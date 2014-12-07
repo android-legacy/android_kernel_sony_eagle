73e495dd5a801733315831fb8bad5b0dde65c1a3
erikcas <erikcas1972@gmail.com>
2014-12-07 22:09:23 +0100

    eagle: fix mirror flip
    Change-Id: I180ca4a726d978bcd1c9eed283ee0d764e6757e3

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -941,8 +941,6 @@ static int mdss_mdp_image_setup(struct mdss_mdp_pipe *pipe,
 	} else
 #endif
 
-        dst_xy = (dst.y << 16) | dst.x;
-
 #ifndef CONFIG_MACH_SONY_EAGLE
 	dst_xy = (dst.y << 16) | dst.x;
 #else
@@ -1074,12 +1072,14 @@ static int mdss_mdp_format_setup(struct mdss_mdp_pipe *pipe)
 	if (pipe->scale.enable_pxl_ext)
 		opmode |= (1 << 31);
 
+#ifndef CONFIG_MACH_SONY_EAGLE
 #ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
 	if (mdss_dsi_panel_flip_ud()) {
 		if (pipe->mfd && pipe->mfd->panel_info &&
 			pipe->mfd->panel_info->pdest == DISPLAY_1)
 			opmode ^= MDSS_MDP_OP_FLIP_UD;
 	}
+#endif
 #endif
 
 	if (fmt->tile && mdata->highest_bank_bit) {
